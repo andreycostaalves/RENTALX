c1b1d9a8065d7d327a734f5cda8b48596cc79b48
Andrey Alves <Andreyalvesgit@gmail.com>
2022-06-07 15:36:40 -0300

    Criando Upload de arquivos com MULTER

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,4 +1,7 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+
 
 
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
@@ -7,12 +10,23 @@ import { listCategoryController } from '../modules/cars/useCases/listCategories'
 
 const categoriesRoutes = Router();
 
+const upLoad = multer({
+  dest: "./tmp",
+})
+
 categoriesRoutes.post('/', (request, response) => {
   return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get('/', (request, response) => {
   return listCategoryController.handle(request, response);
+});
+
+
+categoriesRoutes.post("/import", upLoad.single("file"), (request, response) => {
+  const { file } = request;
+  console.log(file);
+  return response.send();
 })
 
 export { categoriesRoutes }
