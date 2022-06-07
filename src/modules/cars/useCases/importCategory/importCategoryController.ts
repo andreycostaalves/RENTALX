a448e5cf3eabdb76434154a689b18ed1b1988f27
Andrey Alves <Andreyalvesgit@gmail.com>
2022-06-07 15:57:25 -0300

    Criando Use Case para importar categorias

---
+++ src/modules/cars/useCases/importCategory/importCategoryController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+
+import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
+
+
+
+
+class ImportCategoryController {
+
+  constructor(private importeCategoryUseCase: ImportCategoryUseCase) { }
+
+  handle(request: Request, response: Response): Response {
+    const { file } = request;
+
+    this.importeCategoryUseCase.execute(file);
+
+
+    return response.send();
+  }
+}
+
+export { ImportCategoryController };
