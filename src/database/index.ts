a72adf3daecf832ae16e092bc5b2e578b83800de
Andrey Alves <Andreyalvesgit@gmail.com>
2022-06-14 15:32:04 -0300

    TypeOrm - Configurando o typeOrm

---
+++ src/database/index.ts
@@ -0,0 +1,12 @@
+import { DataSource } from "typeorm"
+
+const dataSource = new DataSource({
+  type: "postgres",
+  host: "localhost",
+  username: "",
+  password: "",
+  database: "rentx",
+
+});
+
+dataSource.initialize();
